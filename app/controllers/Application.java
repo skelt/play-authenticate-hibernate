c379fed1e1a7cbc1dce40df7e5962f9d4df4c71e
Stewart Kelt <stewart.kelt@uws.ac.uk>
2015-07-05 23:29:45 +0100

    cleaning code

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -34,7 +34,6 @@ public class Application extends Controller {
 	public static final String FLASH_ERROR_KEY = "error";
 	public static final String USER_ROLE = "user";
 	
-	@Transactional
 	public Result index() {		
 		return ok(index.render());
 	}
